45c795bcbbecdb7e08cf04508064b3c7dc02d1df
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-09-13 09:45:43 -0600

    creating basic html

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,40 @@
-import logo from '../assets/logo.svg';
 import '../App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div id="header">
+        <div id="house">🏠</div>
+        <p id="contact">Contact</p>
+        <p id="help">Help</p>
+        <h2>FBI most wanted</h2>
+      </div>
+      <div id="image-slider">
+        <img id="image1" src="https://images.pexels.com/photos/5811066/pexels-photo-5811066.jpeg"/>
+        <img src="https://www.fbi.gov/wanted/cei/cristian-rios-mora/@@images/image"/>
+        <div id="suspect-info">
+          <h4>Suspect</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p>read more...</p>
+        </div>
+      </div>
+      <div id="select-menu">
+        <div id="first">
+          <img src="https://gen3marketing.com/wp-content/uploads/2021/05/blue-circle.png"/>
+        </div>
+        <div id="second">
+          <img src="https://gen3marketing.com/wp-content/uploads/2021/05/blue-circle.png"/>
+        </div>
+        <div id="third">
+          <img src="https://gen3marketing.com/wp-content/uploads/2021/05/blue-circle.png"/>
+        </div>
+      </div>
+    </>
+
+  )
 }
 
 export default App;
